c1c2b5c8219f6ae8a52b55ef8ac14e49801c5718
Swaroop <saiswaroop2674@gmail.com>
2023-12-20 01:34:12 +0530

    .

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import BackgroundImage from '../components/BackgroundImage';
 import Header from '../components/Header';
 export default function signup() {
-  return <Container>
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [showPassword,setShowPassword] = useState(false);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const[formValues,setFormValues] = useState({
+    email:"",
+    password:"",
+  })
+  return (
+  <Container showPassword={showPassword}>
     <BackgroundImage/>
     <div className="content">
-    <Header/>
-    <div className="body flex colum a-center j-center">
-      <div className="text flex column">
+    <Header login/>
+    <div className="body flex column a-center j-center">
+      <div className="text flex column"> 
         <h1>Unlimited movies, Tv shows and more</h1>
-        <h4>Watch anywheer. Cancel Anytime</h4>
+        <h4>Watch anywhere. Cancel Anytime</h4>
         <h6>
           Ready to watch? Enter your email to create or restart membership
         </h6>
       </div>
       <div className="form">
-        <input type="email" placeholder='Email Address' name="email"/>
-        <input type="password" placeholder='Password' name='password'/>
-        <button>Get Started</button>
+        <input type="email" placeholder='Email Address' name="email" value={formValues.email} onChange={(e)=>setFormValues({...formValues,[e.target.name]:e.target.value})}/>
+        {
+          showPassword && (
+          <input type="password" placeholder='Password' name='password' value={formValues.password} onChange={(e)=>setFormValues({...formValues,[e.target.name]:e.target.value})}/>
+)}
+        
+        {
+          !showPassword && <button onClick={()=>setShowPassword(true)}>Get Started</button>
+        }
+        
       </div>
-      <button>Login</button>
+      <button>Signup</button>
     </div>
     </div>
-  </Container>
+  </Container>)
 }
 
 const Container = styled.div`
@@ -37,5 +52,52 @@ position:relative;
   width:100vw;
   display:grid;
   grid-template-rows:15vh 85vh;
+  .body{
+    gap:1rem;
+    .text{
+      gap:1rem;
+      text-align:center;
+      font-size:2rem;
+      h1{
+        padding:0 25rem;
+      }
+    }
+    .form{
+      display:grid;
+      grid-template-columns: ${({showPassword})=>showPassword ? "1fr 1fr" : "2fr 1fr" };
+      width:60%;
+      input{
+        color:black;
+        border:none;
+        padding:1.5rem;
+        font-size:1.2rem;
+        border:1px solid black;
+        &:focus{
+          outline:none;
+        }
+      }
+      button{
+        padding: 0.5rem 1rem;
+        background-color:#e50914;
+        border:none;
+        cursor:pointer;
+        color:white;
+    
+        font-weight:bolder;
+        font-size: 1.05rem;
+      }
+    }
+    button{
+      padding: 0.5rem 1rem;
+      background-color:#e50914;
+      border:none;
+      cursor:pointer;
+      color:white;
+      border-radius:0.2rem;
+      font-weight:bolder;
+      font-size: 1.05rem;
+    }
+  }
 }
-`
+
+`;
